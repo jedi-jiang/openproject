11a950508113d3792b45a1ea7d1904312d7bd443
Oliver Günther <mail@oliverguenther.de>
2019-04-23 11:21:29 +0200

    [29253] Render error message in production on pending migrations
    Creates a new warning bar (previously top-shelf item) that renders a
    warning when migrations are pending. These did raise exceptions before
    in production and would result in users running in internal errors due
    to pending migration on some actions on the instance.
    
    https://community.openproject.com/wp/29253

--- app/helpers/migrations_helper.rb
+++ app/helpers/migrations_helper.rb
@@ -1,7 +1,8 @@
 #-- encoding: UTF-8
+
 #-- copyright
 # OpenProject is a project management system.
-# Copyright (C) 2012-2018 the OpenProject Foundation (OPF)
+# Copyright (C) 2012-2017 the OpenProject Foundation (OPF)
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License version 3.
@@ -24,25 +25,11 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
-# See docs/COPYRIGHT.rdoc for more details.
+# See doc/COPYRIGHT.rdoc for more details.
 #++
 
-class HomescreenController < ApplicationController
-  skip_before_action :check_if_login_required, only: [:robots]
-
-  layout 'no_menu'
-
-  def index
-    @newest_projects = Project.visible.newest.take(3)
-    @newest_users = User.active.newest.take(3)
-    @news = News.latest(count: 3)
-    @announcement = Announcement.active_and_current
-
-    @homescreen = OpenProject::Static::Homescreen
-    render_500
-  end
-
-  def robots
-    @projects = Project.active.public_projects
+module MigrationsHelper
+  def render_pending_migrations_warning?
+    current_user.admin? && OpenProject::Database.migrations_pending?
   end
 end
